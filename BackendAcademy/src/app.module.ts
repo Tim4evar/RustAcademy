e6b09c338cb35e522d677f719026821186adc4ea
Your Name <you@example.com>
2026-06-27 11:30:53 +0100

    feat: add notifications module skeleton

--- BackendAcademy/src/app.module.ts
+++ BackendAcademy/src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { TutorProfileModule } from './users/tutor-profile.module';
 import { SubmissionModule } from './submissions/submission.module';
 import { RewardsModule } from './rewards/rewards.module';
+import { BadgesModule } from './badges/badges.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [TutorProfileModule, SubmissionModule, RewardsModule],
+  imports: [TutorProfileModule, SubmissionModule, RewardsModule, BadgesModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
